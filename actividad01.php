2c694a4f5a5a431de84e33ddc905a1d34a990af2
PC <PC@10.15.11.23>
2018-10-04 20:57:37 -0500

    Segundo Envio

--- actividad01.php
+++ actividad01.php
@@ -1,8 +1,5 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//App ára imprimir 10#s enteros positivos
+for($i=1;$i<=10;$i++)
+echo "Numero:".$i."<br>";
 
